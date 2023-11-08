65b90a52aa816133dfa3f6f3932bc74bb55be947
Tejas Jaybhaye <117623822+tejayy@users.noreply.github.com>
2023-11-08 13:53:34 +0530

    Added SearchExercises API

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+import { Route, Routes } from 'react-router-dom';
+import { Box } from '@mui/material';
+
+import Navbar from './components/Navbar';
+import Home from './Pages/Home';
+import ExerciseDetail from './Pages/ExerciseDetail';
+import Footer from './components/Footer';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>WELCOME TO FITNESS APP</h1>
+      <Box width="400px" sx={{ width: { xl: "1488px" } }} m="auto">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/exercise/:id" element={<ExerciseDetail />} />
+        </Routes>
+        <Footer />
+      </Box>
     </div>
   );
 }
