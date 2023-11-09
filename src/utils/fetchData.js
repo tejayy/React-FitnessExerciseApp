be7fd63597ff023e03797f96c05cb8b27de21791
Tejas Jaybhaye <117623822+tejayy@users.noreply.github.com>
2023-11-09 11:50:54 +0530

    Final commit

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -6,6 +6,15 @@ export const exerciseOptions = {
   },
 };
 
+
+export const youtubeOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "a71e34a701msh17dc2ee64ab2aa8p1bafd1jsne94c110904c1",
+    "X-RapidAPI-Host": "youtube-search-and-download.p.rapidapi.com",
+  },
+};
+
 export const fetchData = async (url, options) => {
   const response = await fetch(url, options);
   const data = response.json();
